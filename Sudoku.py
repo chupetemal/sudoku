c34f2f0d9298fa7bd252c6519870cc359f008f74
chupete <lautaropiston.lp@gmail.com>
2025-09-26 02:34:06 -0300

    crearcubos

--- Sudoku.py
+++ Sudoku.py
@@ -1,1 +1,5 @@
-
+import random
+def CrearSudoku(Sudoku):
+    CubosSudoku=[[random.randint(1,9)for i in range(3)]for i in range(3)]
+    print(CubosSudoku)
+    print("creando")
--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+from Sudoku import CrearSudoku
+def main():
+    Sudoku=[]
+    CrearSudoku(Sudoku)
 
+
+if __name__ == "__main__":
+    main()
