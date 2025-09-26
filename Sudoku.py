d1c2bf064fd54ef5db82a29cae501b121c82892f
chupete <lautaropiston.lp@gmail.com>
2025-09-26 04:47:09 -0300

    RemoverRepetidos v2

--- Sudoku.py
+++ Sudoku.py
@@ -17,15 +17,18 @@ def RemoverRepetidos(Cubos):
             for l in range(len(Cubos[0])):
                 if i == Cubos[j][l]:
                     Contador+=1
+                    continue
                 else:
-                    if Veces==0:
-                        Veces=1
-                        NumerosFaltantes.append(i)
-                        print(NumerosFaltantes)
-                        
+                    if i not in Cubos[l] and l == j:
+                        Veces+=1
+    
+                    if i not in NumerosFaltantes and Veces==3:
+                            NumerosFaltantes.append(i)
+                            print("nym",NumerosFaltantes)
                     else:
                         continue
-        if Contador>1:
+
+            if Contador>1:
                 print("el numero",i,"esta en la lista",Contador,"veces")
                 Repetidos.append(i)
-                print(Repetidos)
+                print("re",Repetidos)
